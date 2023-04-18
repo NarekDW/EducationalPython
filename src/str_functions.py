743767d0b7edaace3da7c10dc07b2ecbf8951d77
Gagik <35296458+GagikABR@users.noreply.github.com>
2023-04-19 00:12:23 +1000

    commit 1

--- src/str_functions.py
+++ src/str_functions.py
@@ -1,3 +1,8 @@
+from collections import Counter
 class StrFunctions:
-    def get_number_of_longest_identical_characters(self, string: str) -> int:
-        pass
+    def get_number_of_longest_repeated_characters(self, string: str) -> int:
+        a = Counter(string)
+        return string.count(a.most_common(1)[0][0])
+
+
+
